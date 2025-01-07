e4068174b6b7728d7780bd65fd7c9d9e9fb23f8a
Abhay Verma <99489290+AbhayVerma9131@users.noreply.github.com>
2025-01-07 13:11:36 +0530

    Calcuator.java

--- Calcuator.java
+++ Calcuator.java
@@ -5,7 +5,7 @@ import java.lang.Math;
 import java.util.Scanner;
 
 // Driver class
-public class BasicCalculator {
+public class Calculator {
 	// main function
 	public static void main(String[] args)
 	{
